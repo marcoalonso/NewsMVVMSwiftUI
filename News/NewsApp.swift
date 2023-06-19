f8ebf17a0bc604b48d98ad712c00ac363af9ed39
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-06-19 00:14:39 -0600

    Getting started

---
+++ News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by Marco Alonso Rodriguez on 19/06/23.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
