8ef098647a91a5d1e73dfb34bef8af745fc3c37f
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-06-20 20:54:42 -0600

    ViewModel finished with custom alerts handling errors

--- News/ViewModel/NewsViewModel.swift
+++ News/ViewModel/NewsViewModel.swift
@@ -8,10 +8,40 @@
 import Foundation
 
 final class NewsViewModel: ObservableObject {
-    @Published var appetizers: [Noticia] = []
+    @Published var news: [Noticia] = []
     @Published var isLoading: Bool = false
+    @Published var alertItem: AlertItem?
     
-    func getNews() {
+    func getNews(topic: String) {
         isLoading = true
+        
+        NetworkManager.shared.getLisOfNews(topic: topic) { [weak self] result in
+            DispatchQueue.main.async {
+                guard let self else { return }
+                self.isLoading = false
+                switch result {
+                case .success(let news):
+                    self.news = news
+                    
+                case .failure(let error):
+                    switch error {
+                    case .invalidURL:
+                        self.alertItem = AlertContext.invalidURL
+                        
+                    case .decodingError:
+                        self.alertItem = AlertContext.decodingError
+                        
+                    case .invalidData:
+                        self.alertItem = AlertContext.invalidData
+                        
+                    case .invalidResponse:
+                        self.alertItem = AlertContext.invalidResponse
+                        
+                    case .unableToComplete:
+                        self.alertItem = AlertContext.unableToComplete
+                    }
+                }
+            }
+        }
     }
 }
