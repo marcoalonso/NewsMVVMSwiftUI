d26954bb24a94b5448fa6fbfabaee3a7adcc8f1d
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-06-20 22:11:07 -0600

    SearchBar Added

--- News/View/NewsView.swift
+++ News/View/NewsView.swift
@@ -10,12 +10,13 @@ import SwiftUI
 struct NewsView: View {
     
     @StateObject private var viewModel = NewsViewModel()
-    
+    @State private var topicToSearch = ""
   
     
     var body: some View {
         NavigationStack {
             VStack {
+                
                 TopicSegmentedView(viewModel: viewModel)
                 
                 List(viewModel.news, id: \.title) { article in
@@ -26,12 +27,24 @@ struct NewsView: View {
                     LoadingView()
                 }
                 
-            }.onAppear { viewModel.getNews(topic: viewModel.topics.first!)}
+            }
+            .searchable(text: $topicToSearch, prompt: "Write your topic to search news")
+            .onChange(of: topicToSearch, perform: { newTopic in
+                if newTopic != "" {
+                    viewModel.getNews(topic: newTopic)
+                }
+            })
+            .onAppear { viewModel.getNews(topic: viewModel.topics.first!)}
             
             .alert(item: $viewModel.alertItem) { alertItem in
                 Alert(title: alertItem.title, message: alertItem.message, dismissButton: alertItem.dismissButton)
             }
             .navigationBarTitle("Top Global News")
+            .navigationBarItems(trailing: NavigationLink(destination: {
+                EmptyView()
+            }, label: {
+                Image(systemName: "ellipsis.circle")
+            }))
         }
     }
 }
